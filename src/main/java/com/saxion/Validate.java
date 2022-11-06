b0e901114adfcf259412e2e2569ca3af3030dfe9
kalucky0 <kalucki23@gmail.com>
2022-11-06 21:31:57 +0100

    Create validation class

---
+++ src/main/java/com/saxion/Validate.java
@@ -0,0 +1,15 @@
+package com.saxion;
+
+public class Validate {
+    public static boolean IsEmailValid(String email) {
+        return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+    }
+
+    public static boolean IsDateValid(String date) {
+        return date.matches("^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-([0-9]{4})$");
+    }
+
+    public static boolean IsPhoneNumberValid(String phoneNumber) {
+        return phoneNumber.matches("^\\+?[0-9]{3}-?[0-9]{6,12}$");
+    }
+}
---
+++ src/test/java/ValidateTest.java
@@ -0,0 +1,29 @@
+import com.saxion.Validate;
+import org.junit.jupiter.api.Test;
+
+public class ValidateTest {
+    @Test
+    public void ValidateEmail() {
+        assert Validate.IsEmailValid("user@example.com");
+        assert Validate.IsEmailValid("test@email.eu");
+        assert !Validate.IsEmailValid("invalid@email");
+        assert !Validate.IsEmailValid("invalid@.com");
+    }
+
+    @Test
+    public void ValidateDate() {
+        assert Validate.IsDateValid("01-01-2021");
+        assert Validate.IsDateValid("31-12-2021");
+        assert !Validate.IsDateValid("32-01-2021");
+        assert !Validate.IsDateValid("01-13-2021");
+        assert !Validate.IsDateValid("01-01-202");
+    }
+
+    @Test
+    public void ValidatePhoneNumber() {
+        assert Validate.IsPhoneNumberValid("+31612345678");
+        assert Validate.IsPhoneNumberValid("0612345678");
+        assert Validate.IsPhoneNumberValid("664702803");
+        assert !Validate.IsPhoneNumberValid("0612345678a");
+    }
+}
